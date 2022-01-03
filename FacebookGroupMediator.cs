3d3900552a9604ea29d36796ef439063add666f0
Anamika_s <anamika.sawhney22@gmail.com>
2022-01-03 11:46:39 +0530

    MediatorPatternDemo

---
+++ FacebookGroupMediator.cs
@@ -0,0 +1,9 @@
+using System;
+
+    public interface FacebookGroupMediator
+    {
+        void SendMessage(string msg, User user);
+        void RegisterUser(User user);
+    }
+
+    
---
+++ User.cs
@@ -0,0 +1,14 @@
+using System;
+
+        public abstract class User
+        {
+            protected FacebookGroupMediator mediator;
+            protected string name;
+            public User(FacebookGroupMediator mediator, string name)
+            {
+                this.mediator = mediator;
+                this.name = name;
+            }
+            public abstract void Send(string message);
+            public abstract void Receive(string message);
+        }
